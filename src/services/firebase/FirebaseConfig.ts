07c1ad32f51483ec8cbd8055b96773f024ace35e
MauricioFloresGonales <47356611+MauricioFloresGonales@users.noreply.github.com>
2024-11-01 21:06:25 -0300

    firebase config usando .env

--- src/services/firebase/FirebaseConfig.ts
+++ src/services/firebase/FirebaseConfig.ts
@@ -8,12 +8,12 @@ import {getStorage} from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA3TczWvBKTtOT0hX22XlQgYX2JCnFg_xk',
-  authDomain: 'loginapppps.firebaseapp.com',
-  projectId: 'loginapppps',
-  storageBucket: 'loginapppps.appspot.com',
-  messagingSenderId: '352647240452',
-  appId: '1:352647240452:web:ca06dc609ac20eb41d0e51',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID',
+  appId: APP_ID,
 };
 
 // Initialize Firebase
